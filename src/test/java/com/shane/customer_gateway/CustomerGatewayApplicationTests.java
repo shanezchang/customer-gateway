db06e59df2fe8671b2d750a1430109caa6290587
shane <shane.z.chang@gmail.com>
2025-02-22 19:03:09 +0800

    feat: init

---
+++ src/test/java/com/shane/customer_gateway/CustomerGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shane.customer_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
